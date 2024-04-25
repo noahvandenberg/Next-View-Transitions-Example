2f78eb507e43eb9b1cdbd864906fe0ba5346ecf9
Noah V <noah@noahvandenberg.com>
2024-04-25 01:03:48 -0700

    view transitions

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -2,13 +2,13 @@ import { Link } from 'next-view-transitions';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
+    <div className='flex min-h-screen flex-col items-center p-24'>
       <p>Next View Transitions Example</p>
       <div>
-        <Link href='/demo'>
+        <Link href='/'>
           <p>Click me</p>
         </Link>
       </div>
-    </main>
+    </div>
   );
 }
