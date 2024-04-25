16963662ded632c5adb56e0cd30057bf97ebb887
Noah V <noah@noahvandenberg.com>
2024-04-25 01:17:37 -0700

    generate random text demo

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,14 +1,23 @@
+import { Button } from '@/components/ui/button';
 import { Link } from 'next-view-transitions';
 
 export default function Home() {
+  function generateRandomWord() {
+    return Math.random().toString(36).substring(2, 7);
+  }
+  function generateRandomText(length: Number) {
+    return Array(length)
+      .fill(0)
+      .map(() => generateRandomWord())
+      .join(' ');
+  }
   return (
-    <div className='flex min-h-screen flex-col items-center p-24'>
-      <p>Next View Transitions Example</p>
-      <div>
-        <Link href='/'>
-          <p>Click me</p>
-        </Link>
-      </div>
+    <div className='flex min-h-screen flex-col items-center p-24 space-y-4'>
+      <p className='text-xl font-semibold'>Next View Transitions Example</p>
+      <Link href='/'>
+        <Button>Go Home</Button>
+      </Link>
+      <p className='font-light italic'>{generateRandomText(500)}</p>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
+import { Button } from '@/components/ui/button';
 import { Link } from 'next-view-transitions';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
-      <p>Next View Transitions Example</p>
-      <div>
-        <Link href='/demo'>
-          <p>Click me</p>
-        </Link>
-      </div>
+    <main className='flex min-h-screen flex-col items-center p-24 space-y-4'>
+      <p className='text-xl font-semibold'>Next View Transitions Example</p>
+      <Link href='/demo'>
+        <Button>Generate Random Text</Button>
+      </Link>
     </main>
   );
 }
